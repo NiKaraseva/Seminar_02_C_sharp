31cacaa826c2ce27ab9e7930edc84147eec8cda7
Nika <nikarasevas@gmail.com>
2022-09-19 22:00:16 +0300

    Удаляем вторую цифру рандомного 3-ёх значного числа

---
+++ Exercise_2/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Программа, которая выводит случайное трёхзначное число и 
+удаляет вторую цифру этого числа*/
+
+int number = new Random().Next(100, 1000);
+
+
+int num1 = number / 100;
+int num2 = number % 10;
+int num3 = num1 * 10 + num2;
+Console.WriteLine($"Первая и вторая цифры числа {number} = {num3}");
